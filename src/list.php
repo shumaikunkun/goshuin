5a721cac8cbb6eff25683a3f431da35fdf4ed19b
shumaikunkun <s1711520@gmail.com>
2020-02-05 08:48:58 +0900

    御朱印リスト表示ページの大枠

--- src/list.php
+++ src/list.php
@@ -1,18 +1,56 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
 <html>
+<?php $img_path="../img/" ?>
 <head>
   <meta charset="utf-8">
-  <title>  </title>
+  <title> 双馬の御朱印日記【メンバー】 </title>
+  <link rel="stylesheet" href="../css/all.css" type="text/css">
+  <link rel="shortcut icon" href="<?php echo $img_path ?>icon.ico">
 </head>
-<body>
-aaa
+<body background="<?php echo $img_path ?>test.jpg">
+  <header class="site-header">
+    <h1 class="site-logo">双馬の御朱印日記</h1>
+    <nav class="gnav">
+      <ul class="gnav__menu">
+        <li class="gnav__menu__item"><a href="matome.php">トップ</a></li>
+        <li class="gnav__menu__item"><a href="">活動</a></li>
+        <li class="gnav__menu__item"><a href="member.php">メンバー</a></li>
+        <li class="gnav__menu__item"><a href="">更新情報</a></li>
+        <li class="gnav__menu__item"><a href="">Twitter</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="hide">
+    <img src="<?php echo $img_path ?>test.jpg" alt="">
+  </div>
+  <div class="hero">
+  </div>
+  <div class="content">
+
 <?php
-  $arr=glob("../img/*");
-  foreach ($arr as $filename) {
-    echo $filename;
+  require('list_hash.php');
+  foreach($all_goshuin as $image => $name){
+?>
+
+<p><?php echo $name ?></p>
+
+
+<?php
   }
 ?>
 
+  </div>
+  <footer class="site-footer">
+    <p class="copyright">&#169;2019 Shumaikun</p>
+  </footer>
 </body>
 </html>
+
+<?php
+  // //imgディレクトリ内の全ファイルを一覧表示
+  // $arr=glob("../img/*");
+  // foreach ($arr as $filename) {
+  //   echo $filename;
+  // }
+?>
---
+++ src/list_hash.php
@@ -0,0 +1,35 @@
+<?php
+
+$all_goshuin=[
+  "001zenkou"=>"善光寺",
+  "002kawanakajima"=>"川中島八幡社",
+  "003suwa"=>"諏訪大社",
+  "004shinano"=>"信濃国分寺",
+  "005sanada"=>"真田神社",
+  "006touji"=>"東寺",
+  "007rengeou"=>"三十三間堂",
+  "008suzumushi"=>"鈴虫寺",
+  "009tenryuu"=>"天龍寺",
+  "010mibu"=>"壬生寺",
+  "011honnou"=>"本能寺",
+  "012seimei"=>"晴明神社",
+  "013fushimi"=>"伏見稲荷大社",
+  #"014kanei"=>"寛永寺",
+  "000kanei"=>"寛永寺",
+  "015uenorinnou"=>"上野輪王寺",
+  "016kiyomizukannon"=>"清水観音堂",
+  "017benten"=>"不忍池弁天堂",
+  "018ueno"=>"上野東照宮",
+  "019yushima"=>"湯島天満宮",
+  "020kanda"=>"神田明神",
+  "021yushimaseidou"=>"湯島聖堂",
+  "022toukyou"=>"東京大神宮",
+  "023yasukuni"=>"靖国神社",
+  "024tougou"=>"東郷神社",
+  "025meiji"=>"明治神宮",
+  "026hanazono"=>"花園神社",
+  "027atsuta"=>"熱田神宮",
+  ""=>"",
+]
+
+?>
